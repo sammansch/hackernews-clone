7bb5cb0cb7c407fca63249a37381664b9d7b454a
sammansch <sammansch@quickenloans.com>
2020-02-17 19:25:56 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+//import './index.css';
+import App from './components/app';
+import GlobalStyles from './styles/global';
+import registerServiceWorker from './serviceWorker';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+const renderApp = () => {
+    ReactDOM.render(
+        <div>
+            <GlobalStyles />
+            <App />
+        </div>
+    );
+};
+
+renderApp();
+
+registerServiceWorker();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
---
+++ src/styles/palette.js
@@ -0,0 +1,18 @@
+// Inside src/styles/palette.js
+// ---
+
+export const colorsDark = {
+    background: '#272727',
+    backgroundSecondary: '#393C3E',
+    text: '#bfbebe',
+    textSecondary: '#848886',
+    border: '#272727'
+};
+
+export const colorsLight = {
+    background: '#EAEAEA',
+    backgroundSecondary: '#F8F8F8',
+    text: '#848886',
+    textSecondary: '#aaaaaa',
+    border: '#EAEAEA'
+};
